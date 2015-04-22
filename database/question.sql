e7b00ca47b6c782688bf10f4d69a59a4d5e11ce8
phamxuancan <xuancanboy90@gmail.com>
2015-04-22 23:15:21 +0700

    upload database

--- database/question.sql
+++ database/question.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50620
 File Encoding         : 65001
 
-Date: 2015-04-20 21:51:42
+Date: 2015-04-22 23:10:05
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -29,10 +29,13 @@ CREATE TABLE `question` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   `sound` varchar(128) DEFAULT NULL,
+  `type` enum('audio','text') DEFAULT 'text',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records of question
 -- ----------------------------
-INSERT INTO `question` VALUES ('1', 'xuancan12', '123456fd', 'fsdf', 'fsf', 'fsfsd', 'a', '2015-04-19 05:07:06', null, '');
+INSERT INTO `question` VALUES ('1', 'xuancan12', '123456fd', 'fsdf', 'fsf', 'fsfsd', 'a', '2015-04-19 05:07:06', null, '', 'text');
+INSERT INTO `question` VALUES ('2', 'xuancan12', '123456fd', 'fsdf', 'fsf', 'fsfsd', 'a', '2015-04-20 03:28:14', '0000-00-00 00:00:00', '', 'text');
+INSERT INTO `question` VALUES ('3', null, '2', 'xuancan12', '123456fd', 'fsdf', 'fs', '0000-00-00 00:00:00', '2015-04-22 23:04:00', '2015-04-20 03:28:14', 'audio');
