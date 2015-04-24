4e481c8b2b27d6cdaad1e212e4dc7a2afa90960f
quanvh <quanvu@mobgame.vn>
2015-04-24 14:21:36 +0700

    update database/question.sql chuẩn

--- database/point.sql
+++ database/point.sql
@@ -29,8 +29,8 @@ CREATE TABLE `point` (
   `user_id` int(5) NOT NULL,
   `point` int(2) NOT NULL,
   `time` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
--- database/question.sql
+++ database/question.sql
@@ -1,23 +1,29 @@
-/*
-Navicat MySQL Data Transfer
+CREATE DATABASE  IF NOT EXISTS `toeic` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+USE `toeic`;
+-- MySQL dump 10.13  Distrib 5.6.13, for Win32 (x86)
+--
+-- Host: 127.0.0.1    Database: toeic
+-- ------------------------------------------------------
+-- Server version	5.6.14
 
-Source Server         : localhost
-Source Server Version : 50620
-Source Host           : localhost:3306
-Source Database       : toeic
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-Target Server Type    : MYSQL
-Target Server Version : 50620
-File Encoding         : 65001
+--
+-- Table structure for table `question`
+--
 
-Date: 2015-04-22 23:10:05
-*/
-
-SET FOREIGN_KEY_CHECKS=0;
--- ----------------------------
--- Table structure for `question`
--- ----------------------------
 DROP TABLE IF EXISTS `question`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `question` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `question` text,
@@ -28,14 +34,29 @@ CREATE TABLE `question` (
   `answer_correct` varchar(2) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
-  `sound` varchar(128) DEFAULT NULL,
-  `type` enum('audio','text') DEFAULT 'text',
+  `sound` varchar(128) DEFAULT 'audio',
+  `type` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of question
--- ----------------------------
-INSERT INTO `question` VALUES ('1', 'xuancan12', '123456fd', 'fsdf', 'fsf', 'fsfsd', 'a', '2015-04-19 05:07:06', null, '', 'text');
-INSERT INTO `question` VALUES ('2', 'xuancan12', '123456fd', 'fsdf', 'fsf', 'fsfsd', 'a', '2015-04-20 03:28:14', '0000-00-00 00:00:00', '', 'text');
-INSERT INTO `question` VALUES ('3', null, '2', 'xuancan12', '123456fd', 'fsdf', 'fs', '0000-00-00 00:00:00', '2015-04-22 23:04:00', '2015-04-20 03:28:14', 'audio');
+--
+-- Dumping data for table `question`
+--
+
+LOCK TABLES `question` WRITE;
+/*!40000 ALTER TABLE `question` DISABLE KEYS */;
+INSERT INTO `question` VALUES (1,'','What is the name of the man? Harry','What is the name of the woman? Samantha','Who has forgotten to buy some coffee? The boss','Why? Because the shop was closed','A','2015-04-23 23:49:48',NULL,'1429858188_5930176.mp3','audio'),(2,'Is Susan ........... home?','in','at','on','under','B','2015-04-23 23:57:48',NULL,'','text'),(3,'What ............ now?','is the time','does the time','is time','is it','B','2015-04-24 00:04:03',NULL,'','text'),(4,'They always go to school ............. bicycle.','with','by','on','in','B','2015-04-24 00:05:23',NULL,'','text'),(5,'','Christmas','Thanksgiving',' New Year','His birthday','C','2015-04-24 00:13:25',NULL,'1429859605_5675659.mp3','audio'),(6,'','They are a waiter and waitress','They are chefs','They work in a supermarket','They work in a bakery','D','2015-04-24 00:15:15',NULL,'1429859715_3342285.mp3','audio');
+/*!40000 ALTER TABLE `question` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-04-24 14:17:57
