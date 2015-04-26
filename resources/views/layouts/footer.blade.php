a1548e2d8f206a1eb6edf2297fc2a6b79a551c61
quanvh <quanvu@mobgame.vn>
2015-04-26 10:11:22 +0700

    update UI front-end

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,6 @@
 @section('footer')
 	<div class="row footer">
-        <div class="col-md-6"><h1>production</h1></div>
-        <div class="col-md-6"><h1>copyright</h1></div>
+        {{--<div class="col-md-6"><h1>production</h1></div>--}}
+        {{--<div class="col-md-6"><h1>copyright</h1></div>--}}
     </div>
 @show
